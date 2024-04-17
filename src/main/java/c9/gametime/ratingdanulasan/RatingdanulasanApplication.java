440b466c2e62822b286bfba016cca64ee099d9b0
Farhan Ramadhan <farcool17@gmail.com>
2024-04-17 20:58:32 +0700

    first commit

---
+++ src/main/java/c9/gametime/ratingdanulasan/RatingdanulasanApplication.java
@@ -0,0 +1,13 @@
+package c9.gametime.ratingdanulasan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatingdanulasanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RatingdanulasanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/c9/gametime/ratingdanulasan/RatingdanulasanApplicationTests.java
@@ -0,0 +1,13 @@
+package c9.gametime.ratingdanulasan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingdanulasanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
