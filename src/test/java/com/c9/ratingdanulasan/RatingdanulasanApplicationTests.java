8578c67b8bf598a9884446db5c61385873fa8896
SyauqiArman <syauqiarmanaya@gmail.com>
2024-05-22 22:32:06 +0700

    Replace code with new version

---
+++ src/test/java/com/c9/ratingdanulasan/RatingdanulasanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c9.ratingdanulasan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingdanulasanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
